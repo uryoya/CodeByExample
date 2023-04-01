bb959740c54a0e71f4a02bd0ab996fee867e85ee
URANO Ryoya <urano.works.mail@gmail.com>
2023-04-01 17:15:27 +0900

    ApolloServerをCommonJSで動かす最小の設定サンプル

---
+++ typescript/libs/apolloserver_commonjs/src/index.ts
@@ -0,0 +1,72 @@
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import cors from 'cors';
+import { json } from 'body-parser';
+import express from 'express';
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+// startStandaloneServer(server, { listen: { port: 4000 } })
+//   .then(({ url }) => {
+//     console.log(`🚀 Server listening at: ${url}`);
+//   })
+
+const app = express();
+const port = 3000;
+
+server
+  .start()
+  .then(() => {
+    app.use('/graphql', cors<cors.CorsRequest>(), json(), expressMiddleware(server));
+    return app.listen(port, () => {
+      console.log(`🚀 Express Server listening at: http://localhost:${port}/graphql`)
+    });
+  });
