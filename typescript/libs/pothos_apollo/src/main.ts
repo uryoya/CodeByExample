6e81d74c618cc2f3b04f636cdb492e740ed3e7a7
URANO Ryoya <urano.works.mail@gmail.com>
2023-11-28 09:55:27 +0900

    pothos-apollo sample

--- typescript/libs/pothos_apollo/src/main.ts
+++ typescript/libs/pothos_apollo/src/main.ts
@@ -1,8 +1,49 @@
 import SchemaBuilder from "@pothos/core";
+import TracingPlugin, {
+  wrapResolver,
+  isRootField,
+} from "@pothos/plugin-tracing";
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-const builder = new SchemaBuilder({});
+export class Post {
+  id: string;
+  title: string;
+  content: string;
+
+  constructor(id: string, title: string, content: string) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+  }
+}
+
+const builder = new SchemaBuilder({
+  plugins: [TracingPlugin],
+  tracing: {
+    default: (config) => isRootField(config),
+    wrap: (resolver, options, config) =>
+      wrapResolver(resolver, (error, duration) => {
+        console.log(
+          `Executed resolver ${config.parentType}.${config.name} in ${duration}ms`,
+        );
+      }),
+  },
+});
+
+builder.objectType(Post, {
+  name: "Post",
+  description: "A blog post",
+  fields: (t) => ({
+    id: t.exposeID("id"),
+    title: t.exposeString("title", {
+      description: "The title of the blog post",
+    }),
+    content: t.exposeString("content", {
+      description: "The content of the blog post",
+    }),
+  }),
+});
 
 builder.queryType({
   fields: (t) => ({
@@ -11,6 +52,24 @@ builder.queryType({
         name: t.arg.string(),
       },
       resolve: (parent, { name }) => `hello, ${name || "World"}`,
+    }),
+    post: t.field({
+      type: Post,
+      resolve: () => new Post("1", "Hello World", "This is a blog post."),
+    }),
+  }),
+});
+
+builder.mutationType({
+  fields: (t) => ({
+    post: t.boolean({
+      args: {
+        message: t.arg.string(),
+      },
+      resolve: (root, args) => {
+        // do something
+        return true;
+      },
     }),
   }),
 });
