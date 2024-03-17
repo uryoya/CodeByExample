3a2823de949e719573548b7204f944dd9656a32a
URANO Ryoya <urano.works.mail@gmail.com>
2024-03-17 22:28:26 +0900

    Rustの練習帳：第2章

---
+++ rust/command-line-rust/echor/tests/cli.rs
@@ -0,0 +1,56 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+use std::fs;
+
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
+#[test]
+fn dies_no_args() -> TestResult {
+    Command::cargo_bin("echor")?
+    .assert()
+    .failure()
+    .stderr(predicate::str::contains("USAGE"));
+    
+    Ok(())
+}
+
+#[test]
+fn runs() -> TestResult {
+    Command::cargo_bin("echor")?
+        .arg("hello")
+        .assert()
+        .success();
+    
+    Ok(())
+}
+
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("echor")?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+    
+    Ok(())
+}
+
+#[test]
+fn hello1() -> TestResult {
+    run(&["Hello there"], "tests/expected/hello1.txt")
+}
+
+#[test]
+fn hello2() -> TestResult {
+    run(&["Hello", "there"], "tests/expected/hello2.txt")
+}
+
+#[test]
+fn hello1_no_newline() -> TestResult {
+    run(&["Hello  there", "-n"], "tests/expected/hello1.n.txt")
+}
+
+#[test]
+fn hello2_no_newline() -> TestResult {
+    run(&["Hello", "there", "-n"], "tests/expected/hello2.n.txt")
+}
