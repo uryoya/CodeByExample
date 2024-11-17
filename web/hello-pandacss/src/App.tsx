8d6c2a80f7abe227c8eb36f9c8847b4b8836aeb5
URANO Ryoya <urano.works.mail@gmail.com>
2024-11-17 21:57:58 +0900

    pattern sample: stack, hstack, vstack

--- web/hello-pandacss/src/App.tsx
+++ web/hello-pandacss/src/App.tsx
@@ -1,6 +1,15 @@
+import React from "react";
 import { css } from "../styled-system/css";
-import { Box, Container } from "../styled-system/jsx";
-import { container } from "../styled-system/patterns";
+import { Box, Container, HStack, Stack, VStack } from "../styled-system/jsx";
+import { container, hstack, stack, vstack } from "../styled-system/patterns";
+
+type SectionTitleProps = {
+  children?: React.ReactNode;
+};
+
+const SectionTitle: React.FC<SectionTitleProps> = ({ children }) => (
+  <h2 className={css({ bg: "gray.200", fontSize: "xl" })}>{children}</h2>
+);
 
 function App() {
   return (
@@ -19,7 +28,7 @@ function App() {
        *
        * @see https://panda-css.com/docs/concepts/patterns#box
        */}
-      <h2>Box</h2>
+      <SectionTitle>Box</SectionTitle>
       <Box color="blue.300">
         <div>Cool!</div>
       </Box>
@@ -38,7 +47,7 @@ function App() {
        *
        * @see https://panda-css.com/docs/concepts/patterns#container
        */}
-      <h2>Container</h2>
+      <SectionTitle>Container</SectionTitle>
 
       <h3>function style</h3>
       <div className={container()}>
@@ -53,6 +62,80 @@ function App() {
         <div>Second</div>
         <div>Third</div>
       </Container>
+
+      {/**
+       * Stack
+       *
+       * スタックパターンは、水平または垂直のスタックを作成するために使用される。
+       * Stackは次のプロパティを受け付ける。
+       * - direction: An alias for the css flex-direction property. Default is column.
+       * - gap: The gap between the elements in the stack.
+       * - align: An alias for the css align-items property.
+       * - justify: An alias for the css justify-content property.
+       *
+       * @see https://panda-css.com/docs/concepts/patterns#stack
+       */}
+      <SectionTitle>Stack</SectionTitle>
+
+      <h3>function style</h3>
+      <div className={stack({ gap: "6", padding: "4" })}>
+        <div>First</div>
+        <div>Second</div>
+        <div>Third</div>
+      </div>
+
+      <h3>JSX style</h3>
+      <Stack gap="6" padding="4">
+        <div>First</div>
+        <div>Second</div>
+        <div>Third</div>
+      </Stack>
+
+      {/**
+       * HStack
+       *
+       * HStackパターンはStackパターンのラッパーで、directionプロパティがhorizontalに設定され、要素を垂直方向に中央揃えにする。
+       *
+       * @see https://panda-css.com/docs/concepts/patterns#hstack
+       */}
+      <SectionTitle>HStack</SectionTitle>
+
+      <h3>function style</h3>
+      <div className={hstack({ gap: "6" })}>
+        <div>First</div>
+        <div>Second</div>
+        <div>Third</div>
+      </div>
+
+      <h3>JSX style</h3>
+      <HStack gap="6">
+        <div>First</div>
+        <div>Second</div>
+        <div>Third</div>
+      </HStack>
+
+      {/**
+       * VStack
+       *
+       * VStackパターンはStackパターンのラッパーで、directionプロパティがverticalに設定され、要素を水平方向に中央揃えにする。
+       *
+       * @see https://panda-css.com/docs/concepts/patterns#vstack
+       */}
+      <SectionTitle>VStack</SectionTitle>
+
+      <h3>function style</h3>
+      <div className={vstack({ gap: "6" })}>
+        <div>First</div>
+        <div>Second</div>
+        <div>Third</div>
+      </div>
+
+      <h3>JSX style</h3>
+      <VStack gap="6">
+        <div>First</div>
+        <div>Second</div>
+        <div>Third</div>
+      </VStack>
     </>
   );
 }
