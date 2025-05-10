839f66d546a7e2842ca0253154e9058ae7357013
URANO Ryoya <urano.works.mail@gmail.com>
2025-05-10 14:54:15 +0900

    MSTestをxUnit.netに変更しFluentAssertionsを導入

---
+++ csharp/lang/LangFeature.Tests/SampleTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+using LangFeature;  // テスト対象の名前空間
+using FluentAssertions;
+
+namespace LangFeature.Tests
+{
+    public class SampleTests
+    {
+        [Fact]
+        public void 足し算が正しく動く()
+        {
+            // Arrange
+            var calc = new Calculator();  // 仮にLangFeatureにあるクラス
+
+            // Act
+            var result = calc.Add(2, 3);
+
+            // Assert
+            Assert.Equal(5, result);
+
+            result.Should().Be(5);
+        }
+    }
+}
+
+class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
